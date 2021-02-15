c5092e7ab0da8af38502b9aeee2baf3793be84a9
Shuanglin.Deng <shuanglindev@gmail.com>
2021-02-15 14:28:46 +0800

    project init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{
+      height:'100%',
+      // backgroundImage: `url(${logo})` 
+    }} >
+      test111
+    </div>
+  );
+}
+
+export default App;
